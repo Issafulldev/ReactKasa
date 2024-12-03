a8d03092634abf7539b8b4a98a818a2951445811
Issafulldev <aissa.azgr@protonmail.com>
2024-12-03 22:36:46 +0700

    github pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import * as path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ReactKasa/',
   server: {
     host: '0.0.0.0', // Permet d'accepter des connexions depuis n'importe quelle adresse IP
     port: 3000 // Le port que tu souhaites utiliser
